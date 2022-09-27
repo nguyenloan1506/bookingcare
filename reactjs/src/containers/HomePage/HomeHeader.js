4613b8d5fe05a12167dcb6efa34eff95b46bc432
nguyenloan <20020114@vnu.edu.vn>
2022-09-28 00:50:58 +0700

    redux, slider

--- reactjs/src/containers/HomePage/HomeHeader.js
+++ reactjs/src/containers/HomePage/HomeHeader.js
@@ -2,64 +2,99 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import "./HomeHeader.scss";
+import logo from "../../assets/images/logo.svg";
+import { FormattedMessage } from "react-intl";
+import {LANGUAGES} from "../../utils"
+import {changeLanguageApp} from "../../store/actions";
 
 class HomeHeader extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  }
+
   render() {
+    let language = this.props.language;
+
     return (
       <React.Fragment>
         <div className="home-header-container">
           <div className="home-header-content">
             <div className="left-content">
               <i className="fa-solid fa-bars"></i>
-              <div className="header-logo"></div>
+              <img className="header-logo" src={logo} />
             </div>
 
             <div className="center-content">
               <div className="child-content">
                 <div>
-                  <b>Chuyen khoa</b>
+                  <b>
+                    <FormattedMessage id="homeheader.speciality" />
+                  </b>
                 </div>
-                <div className="sub-title">Tim bac si theo chuyen khoa</div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.searchdoctor" />
+                </div>
               </div>
 
               <div className="child-content">
                 <div>
-                  <b>Chuyen khoa1</b>
+                  <b>
+                    <FormattedMessage id="homeheader.health-facility" />
+                  </b>
                 </div>
-                <div className="sub-title">Tim bac si theo chuyen khoa</div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.select-room" />
+                </div>
               </div>
 
               <div className="child-content">
                 <div>
-                  <b>Chuyen khoa2</b>
+                  <b>
+                    <FormattedMessage id="homeheader.doctor" />
+                  </b>
                 </div>
-                <div className="sub-title">Tim bac si theo chuyen khoa</div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.select-doctor" />
+                </div>
               </div>
 
               <div className="child-content">
                 <div>
-                  <b>Chuyen khoa3</b>
+                  <b>
+                    <FormattedMessage id="homeheader.fee" />
+                  </b>
                 </div>
-                <div className="sub-title">Tim bac si theo chuyen khoa</div>
+                <div className="subs-title">
+                  <FormattedMessage id="homeheader.check-health" />
+                </div>
               </div>
             </div>
 
             <div className="right-content">
               <div className="support">
                 <i className="fa-solid fa-circle-question"></i>
-                Ho tro
+                <FormattedMessage id="homeheader.support" />
               </div>
 
-              <div className="flag">VN</div>
+              <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}>
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VI</span>
+              </div>
+              <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}>
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="home-header-banner">
           <div className="content-up">
-            <div className="title1">NỀN TẢNG Y TẾ</div>
+            <div className="title1">
+              <FormattedMessage id="banner.title1" />
+            </div>
 
-            <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+            <div className="title2">
+              <FormattedMessage id="banner.title2" />
+            </div>
 
             <div className="search">
               <i class="fa-solid fa-magnifying-glass"></i>
@@ -74,7 +109,9 @@ class HomeHeader extends Component {
                   <i className="fa-solid fa-hospital"></i>
                 </div>
 
-                <div className="text-child">Khám Chuyên khoa</div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child1" />
+                </div>
               </div>
 
               <div className="option-child">
@@ -82,7 +119,9 @@ class HomeHeader extends Component {
                   <i className="fa-solid fa-mobile-screen"></i>
                 </div>
 
-                <div className="text-child">Khám tu xa</div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child2" />
+                </div>
               </div>
 
               <div className="option-child">
@@ -90,7 +129,9 @@ class HomeHeader extends Component {
                   <i className="fa-solid fa-bed-pulse"></i>
                 </div>
 
-                <div className="text-child">Khám tong quat</div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child3" />
+                </div>
               </div>
 
               <div className="option-child">
@@ -98,7 +139,9 @@ class HomeHeader extends Component {
                   <i className="fa-solid fa-microscope"></i>
                 </div>
 
-                <div className="text-child">xet nghiem y hoc</div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child4" />
+                </div>
               </div>
 
               <div className="option-child">
@@ -106,7 +149,9 @@ class HomeHeader extends Component {
                   <i className="fa-solid fa-user-doctor"></i>
                 </div>
 
-                <div className="text-child">suc khoe tam than</div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child5" />
+                </div>
               </div>
 
               <div className="option-child">
@@ -114,7 +159,9 @@ class HomeHeader extends Component {
                   <i className="fa-solid fa-tooth"></i>
                 </div>
 
-                <div className="text-child">kham nha khoa</div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child6" />
+                </div>
               </div>
             </div>
           </div>
@@ -127,11 +174,16 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    changeLanguageAppRedux: (language) => {
+      dispatch(changeLanguageApp(language))
+    }
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeHeader);
---
+++ reactjs/src/containers/HomePage/Section/Specialty.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Specialty.scss";
+import { FormattedMessage } from "react-intl";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    >
+      {" "}
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    >
+      {" "}
+    </div>
+  );
+}
+
+class Specialty extends Component {
+  render() {
+    let settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+    };
+
+    return (
+      <div className="section-specialty">
+        <div className="specialty-container">
+          <div className="specialty-header">
+            <span className="title-section">chuyen khoa pho bien</span>
+            <button className="btn-section">xem them</button>
+          </div>
+
+          <div className="specialty-body">
+            <Slider {...settings}>
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 1</div>
+              </div>
+
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 2</div>
+              </div>
+
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 3</div>
+              </div>
+
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 4</div>
+              </div>
+
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 5</div>
+              </div>
+
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 6</div>
+              </div>
+
+              <div className="specialty-customize">
+                <div className="bg-image" />
+                <div>co xuong khop 7</div>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
